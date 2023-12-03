859f9090059d7d82ce17740f5f190aed2f52af48
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2023-12-04 00:46:18 +0100

    models update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_232645) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_03_234047) do
+  create_table "applications", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_applications_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_03_232645) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "applications", "users"
 end
