5f78ac3383c4dc57d8238f9d578c7e134d3640d7
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2023-12-04 01:09:11 +0100

    more fields

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,5 @@
 class Application < ApplicationRecord
   belongs_to :user
 
+  enum status: { pending: 0, approved: 1, rejected: 2, open: 4, closed: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_234047) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_000708) do
   create_table "applications", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "company"
+    t.string "url"
+    t.string "status"
     t.index ["user_id"], name: "index_applications_on_user_id"
   end
 
@@ -28,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_03_234047) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
