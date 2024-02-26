083c2068c58f5354e289a0bb194cad47c928065e
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2024-02-26 02:15:35 +0100

    Merge f/register

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable,
+         authentication_keys: [:username]
 
   has_many :applications
+  def email_required?
+    false
+  end
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_000708) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_23_091055) do
   create_table "applications", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -24,7 +24,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_000708) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -32,7 +31,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_000708) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
