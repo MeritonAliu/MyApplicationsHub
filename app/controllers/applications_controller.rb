c3ad0f7c4783e9f39646aba65c26e61803f9fd90
MeritonAliu <77863815+MeritonAliu@users.noreply.github.com>
2024-06-27 21:37:41 +0200

    finish

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -66,6 +66,6 @@ class ApplicationsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def application_params
-      params.require(:application).permit(:name, :location, :company, :url, :status, :user_id)
+      params.require(:application).permit(:name, :location, :company, :url, :status, :description, :user_id)
     end
 end
